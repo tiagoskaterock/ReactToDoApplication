3b3268a9b359f18640042b4adaf5c2f61636cf24
tiago.inovatech <tiago.inovatech.inf@gmail.com>
2023-07-17 23:30:12 -0300

    Using state

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Download the .zip and unziped it or clone the project
 
 Any feedback or suggestion will be appreciated. ^^
 
-The Class is here: [Lear React with this One Project](https://www.youtube.com/watch?v=Rh3tobg7hEo)
+The Class is here: [Learn React with this One Project](https://www.youtube.com/watch?v=Rh3tobg7hEo)
 
 <!-- ![Calculator](public/todo/1.png) -->
--- src/todo/components/ToDo.jsx
+++ src/todo/components/ToDo.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react"
+import "../css/main.css"
+
 export default function ToDo()  {
+    
+    const [newItem, setNewItem] = useState("")
+
     return (
-        <h1>To Do Application</h1>
+        <>
+            <form className="new-item-form">
+                <div className="form-row">
+                    <label htmlFor="item">New Item</label>
+                    <input 
+                        value={newItem} 
+                        onChange={e => setNewItem(e.target.value)} 
+                        type="text" 
+                        id="item" 
+                        placeholder="Your new item"
+                    />
+                </div>
+
+                <button className="btn">Add</button>
+            </form>
+            <h1 className="header">To Do List</h1>
+
+            <ul className="list">
+                <li>
+                    <label htmlFor="checkbox1">
+                        <input type="checkbox" id="checkbox1" />
+                        Item 1
+                    </label>
+
+                    <button className="btn btn-danger">
+                        Delete
+                    </button>
+                </li>
+
+                <li>
+                    <label htmlFor="checkbox2">
+                        <input type="checkbox" id="checkbox2" />
+                        Item 2
+                    </label>
+
+                    <button className="btn btn-danger">
+                        Delete
+                    </button>
+                </li>
+                
+            </ul>
+        </>
     )
 }
